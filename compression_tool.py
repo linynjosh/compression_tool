79e7231904c4a5c56b2ce0711adbec3e28505f54
Josh Lin <linynjosh@gmail.com>
2024-01-01 19:02:21 -0800

    Step 2: In this step your goal is to use the frequencies that you calculated in step 1 to build the binary tree. There’s a good explanation of how to do this complete with a visualisation here: https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/Huffman.html

--- compression_tool.py
+++ compression_tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from abc import ABC, abstractmethod
 
 def main():
     parser = argparse.ArgumentParser(
@@ -14,7 +15,7 @@ def main():
     char_occurrence_freq = {}
 
     file = open(args.filename, "r")
-    
+
     for line in file:
         for char in line:
             if char not in char_occurrence_freq:
@@ -26,3 +27,69 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
+class HuffBaseNode(ABC):
+    @abstractmethod
+    def isLeaf(self):
+        pass
+
+    @abstractmethod
+    def weight(self):
+        pass
+
+class HuffLeafNode(HuffBaseNode):
+    def __init__(self, element, weight):
+        self.element = element
+        self.weight = weight
+
+    def value(self):
+        return self.element
+
+    def weight(self):
+        return self.weight
+    
+    def isLeaf():
+        return True
+    
+
+class HuffInternalNode(HuffBaseNode):
+    def __init__(self, weight, left, right):
+        self.weight = weight
+        self.left = left
+        self.right = right
+
+    def left(self):
+        return self.left
+    
+    def right(self):
+        return self.right
+
+    def weight(self):
+        return self.weight
+    
+    def isLeaf():
+        return True
+    
+class HuffTree:
+    def __init__(self, el=None, wt=None, l=None, r=None):
+        if el is not None and wt is not None:
+            self.root = HuffLeafNode(el, wt)
+        elif l is not None and r is not None and wt is not None:
+            self.root = HuffInternalNode(l, r, wt)
+        else:
+            raise ValueError("Invalid arguments for creating HuffTree")
+
+    def root(self):
+        return self.root
+
+    def weight(self):
+        return self.root.weight()
+
+    def __lt__(self, other):
+        if self.root.weight() < other.weight():
+            return True
+        elif self.root.weight() == other.weight():
+            return False
+        else:
+            return False
